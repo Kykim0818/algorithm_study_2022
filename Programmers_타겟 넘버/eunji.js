7bdf0a120d5f4e2ba1456ededc8c4aab2a0d56fd
Eunji <ejhwang@seegene.com>
2022-05-18 21:55:25 +0900

    Update and rename eunji(시간초과).js to eunji.js

--- Programmers_타겟 넘버/eunji.js
+++ Programmers_타겟 넘버/eunji.js
@@ -2,20 +2,13 @@ function solution(numbers, target) {
     let q=[];
     let count=0;
     
-    let sum=0;
-    for(let num in numbers){
-        sum+=num;
-    }
-    if(sum<target){
-        return 0
-    }
-    
     q.push({num:numbers[0],index:0});
     q.push({num:-numbers[0],index:0});
+    let ii=0;
     
-    while(q.length!==0){
-        
-        let next=q.shift();
+    while(ii<q.length){
+        // let next=q.shift();
+        let next=q[ii++];
         let nextNum=next.num;
         let nextIndex=next.index;
         
@@ -28,4 +21,3 @@ function solution(numbers, target) {
              
     }
     return count;
-}
